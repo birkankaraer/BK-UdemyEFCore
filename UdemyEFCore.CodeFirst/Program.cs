dac5eaf34a3599ba731d53ea19b06dad204bb4c5
Birkankaraer <karaermustafabirkan@gmail.com>
2023-12-13 20:43:08 +0300

    Add project files.

---
+++ UdemyEFCore.CodeFirst/Program.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using UdemyEFCore.CodeFirst;
+using UdemyEFCore.CodeFirst.DAL;
+
+Initializer.Build();
+using (var _context = new AppDbContext())
+{
+    var produtcs = await _context.Products.ToListAsync();
+
+    produtcs.ForEach(p =>
+    {
+        Console.WriteLine($"{p.Id}: {p.Name} - {p.Price} - {p.Stock}");
+        //{p.Stock.hasvalue} kodu ile değerin null olup olmadığını kontrol edebiliriz
+    });
+}
---
+++ UdeymEFCore.DatabaseFirstByScaffold/Program.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+using UdeymEFCore.DatabaseFirstByScaffold.Models;
+
+using (var context = new UdemyEfcoreDatabaseFirstDbContext())
+{
+    var produtcs = await context.Products.ToListAsync();
+
+    produtcs.ForEach(p =>
+    {
+        Console.WriteLine($"{p.Id}: {p.Name} - {p.Price} - {p.Stock}");
+        //{p.Stock.hasvalue} kodu ile değerin null olup olmadığını kontrol edebiliriz
+    });
+}
